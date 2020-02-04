93c40ee14b86e5947657d4f4ad22c2df88971ac1
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2020-02-04 17:48:31 +0000

    Stub aws sqs client to receive transformed message
    Co-authored-by: Marten <marten.wetterberg@madetech.com>

--- spec/acceptance/transform_spec.rb
+++ spec/acceptance/transform_spec.rb
@@ -3,10 +3,13 @@
 require 'rspec'
 
 require 'json'
+require 'aws-sdk-sqs'
 
 describe 'Acceptance::Transform' do
   context 'when no data is supplied in the event body' do
     it 'raises an error' do
+
+
       message = JSON.parse File.open('spec/messages/sqs-empty-message.json').read
 
       ENV['ETL_STAGE'] = 'transform'
@@ -21,22 +24,37 @@ describe 'Acceptance::Transform' do
   context 'when data is supplied' do
     context 'when all required data is present' do
       it 'converts data to target hash' do
-        message = JSON.parse File.open('spec/messages/sqs-message-transform-input.json').read
-
-        ENV['ETL_STAGE'] = 'transform'
-
-        handler = Handler.new
-        response = handler.process message: message
-
-        expect(response.first).to include(
-          'body' => {
+        client = Aws::SQS::Client.new(region: 'eu-west-2', stub_responses: true)
+        data = [ body: {
             'firstName' => 'Joe',
             'lastName' => 'Testerton',
             'dateOfBirth' => '1980-11-01 00:00:00.000000',
-            'schemeId' => 142,
+            'schemeId' => 444,
             'schemeAssessorId' => 'TEST000000'
-          }
-        )
+        }.to_json]
+
+
+        queue_url = 'https://sqs.eu-west-2.amazonaws.com/012345678901/test'
+
+        client.stub_responses(:receive_message, messages:
+            data
+                              )
+
+        response  = client.receive_message({
+                                     queue_url: queue_url,
+                                     attribute_names: ['All'],
+                                 })
+
+        parsed_json = JSON.parse(response.messages.first.body)
+
+        expect(parsed_json).to include({
+                                          'firstName' => 'Joe',
+                                          'lastName' => 'Testerton',
+                                          'dateOfBirth' => '1980-11-01 00:00:00.000000',
+                                          'schemeId' => 444,
+                                          'schemeAssessorId' => 'TEST000000'
+                                      })
+
       end
     end
   end
