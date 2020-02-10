6e95de0c6ee338d340790d128d0a238eba6e67c1
Yusuf <yusuf@madetech.com>
2020-02-10 12:13:09 +0000

    Refactor: Remove extract output JSON file and redundant acceptance test

--- spec/acceptance/extract_spec.rb
+++ spec/acceptance/extract_spec.rb
@@ -54,12 +54,63 @@ describe 'Acceptance::Extract' do
   end
 
   context 'when data is supplied in the event body' do
+    let(:oracle_adapter) do
+      OracleAdapterFake.new [
+        {
+          ASSESSOR_KEY: 89_030_012,
+          "ACTIVE_IND": 'N',
+          "ASSESSOR_ID": 'TEST000000',
+          "BASE_POSTCODE_LOCATION": 'S1 1DA',
+          "ADDRESS1": 'Green House',
+          "ADDRESS2": '89 Street',
+          "ADDRESS3": nil,
+          "POST_TOWN": 'Sheffield',
+          "POSTCODE": 'S1 2HE',
+          "COMPANY_NAME": 'Test Ltd',
+          "EMAIL": 'test@example.com',
+          "COMPANY_FAX": nil,
+          "TELEPHONE": '07777777777',
+          "COMPANY_WEBSITE": 'www.example.com',
+          "DATE_OF_BIRTH": '1980-11-01 00:00:00.000000',
+          "FIRST_NAME": 'Joe',
+          "NAME_PREFIX": 'Mr',
+          "NAME_SUFFIX": nil,
+          "SURNAME": 'Testerton',
+          "DELETED_IN_UPLOAD": 123_456,
+          "ORGANISATION_KEY": 444,
+          "FIRST_NAME_PHONIC": 'JM',
+          "SURNAME_PHONIC": 'TN',
+          "ASSESSOR_LOCATION": [
+            1234,
+            1234,
+            [
+              746_745,
+              523_646,
+              nil
+            ],
+            nil,
+            nil
+          ],
+          "MIDDLE_NAMES": 'Not Applicable',
+          "DISPLAY_NAME": nil,
+          "COMPANY_REG_NO": nil,
+          "COMPANY_ADDR1": 'Example address',
+          "COMPANY_ADDR2": 'Long Street',
+          "COMPANY_ADDR3": nil,
+          "COMPANY_TOWN": 'Sheffield',
+          "COMPANY_POSTCODE": 'S1 1DA',
+          "COMPANY_TELEPHONE": '07000 678901',
+          "COMPANY_EMAIL": 'test@example.com',
+          "UPI": nil
+        }
+      ]
+    end
+
     it 'extracts the data' do
       message = JSON.parse File.open('spec/messages/sqs-message-extract-input.json').read
 
       ENV['ETL_STAGE'] = 'extract'
 
-      oracle_adapter = OracleAdapterFake.new([{ 'FIRST_NAME': 'Joe' }])
       sqs_adapter = SqsAdapterFake.new
 
       message_gateway = Gateway::MessageGateway.new(sqs_adapter)
@@ -72,36 +123,7 @@ describe 'Acceptance::Extract' do
       handler = Handler.new(container)
       handler.process message: message
 
-      expected_extract_output = JSON.parse File.open('spec/messages/sqs-message-extract-output.json').read
-
-      expect(sqs_adapter.read).to eq(expected_extract_output)
-    end
-  end
-
-  context 'when data is supplied in the event body' do
-    it 'extracts the data, leaving array as seen in database' do
-      message = JSON.parse File.open('spec/messages/sqs-message-extract-input.json').read
-      message['Records'][0]['body']['configuration']['extract']['queries']['ASSESSOR']['multiple'] = true
-
-      ENV['ETL_STAGE'] = 'extract'
-
-      oracle_adapter = OracleAdapterFake.new([{ 'FIRST_NAME': 'Joe' }])
-      sqs_adapter = SqsAdapterFake.new
-
-      message_gateway = Gateway::MessageGateway.new(sqs_adapter)
-      database_gateway = Gateway::DatabaseGateway.new(oracle_adapter)
-
-      container = Container.new
-      container.set_object(:message_gateway, message_gateway)
-      container.set_object(:database_gateway, database_gateway)
-
-      handler = Handler.new(container)
-
-      handler.process message: message
-
-      expected_extract_output = JSON.parse File.open('spec/messages/sqs-message-extract-output.json').read
-      expected_extract_output['Records'][0]['body']['configuration']['extract']['queries']['ASSESSOR']['multiple'] = true
-      expected_extract_output['Records'][0]['body']['data']['ASSESSOR'] = JSON.parse([{ 'FIRST_NAME': 'Joe' }].to_json)
+      expected_extract_output = JSON.parse File.open('spec/messages/sqs-message-transform-input.json').read
 
       expect(sqs_adapter.read).to eq(expected_extract_output)
     end
