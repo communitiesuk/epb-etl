7634ac0b7f8a8c358e63a84a5c6795c4e4172523
Christine-horrocks <splogy2001@hotmail.com>
2020-02-10 16:36:34 +0000

    Add message gateway test for write method
    Co-authored-by: Yusuf <yusuf@madetech.com>

--- spec/unit/gateway/message_gateway_spec.rb
+++ spec/unit/gateway/message_gateway_spec.rb
@@ -29,5 +29,14 @@ describe Gateway::MessageGateway do
         ]
       )
     end
+
+    it 'modifies the message' do
+      sqs_adapter = SqsAdapterFake.new
+
+      message_gateway = Gateway::MessageGateway.new(sqs_adapter)
+      response = message_gateway.write('Testy testington')
+
+      expect(response).to eq('Testy testington')
+    end
   end
 end
