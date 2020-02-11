9d5d356763fd27caa08867503b0db286a17f7162
Yusuf <yusuf@madetech.com>
2020-02-11 14:12:37 +0000

    Refactor: remove message when raising exception

--- lib/gateway/database_gateway.rb
+++ lib/gateway/database_gateway.rb
@@ -9,7 +9,7 @@ module Gateway
     def read(query)
       result = @oracle_adapter.read(query['query'])
 
-      raise Errors::ResultEmpty.new, 'There are no results for this query' if result == nil
+      raise Errors::ResultEmpty if result.nil?
 
       return result[0] if result && !query['multiple']
 
