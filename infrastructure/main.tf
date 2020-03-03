d0f67b64cfa069890421f9462624134a1037c5f7
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2020-03-03 15:45:38 +0000

    Add both transform and load stages to the deployment
    Co-authored-by: Yusuf <yusuf@madetech.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,3 +18,29 @@ module "extract_stage" {
   service_tags  = var.service_tags
   stage         = "extract"
 }
+
+module "transform_stage" {
+  source        = "./stage"
+  handler       = data.remotefile_read.handler
+  layers        = [
+    {
+      name = "bundler"
+      source = data.remotefile_read.bundler_layer
+    }
+  ]
+  service_tags  = var.service_tags
+  stage         = "transform"
+}
+
+module "load_stage" {
+  source        = "./stage"
+  handler       = data.remotefile_read.handler
+  layers        = [
+    {
+      name = "bundler"
+      source = data.remotefile_read.bundler_layer
+    }
+  ]
+  service_tags  = var.service_tags
+  stage         = "load"
+}
