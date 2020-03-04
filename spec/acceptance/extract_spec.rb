0549d327fb09cf68da7a3847f752e54a6a2df369
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2020-03-04 11:44:36 +0000

    Refactor: Move oracle data into a stub
    Co-authored-by: Yusuf <yusuf@madetech.com>

--- spec/acceptance/extract_spec.rb
+++ spec/acceptance/extract_spec.rb
@@ -16,55 +16,7 @@ describe 'Acceptance::Extract' do
 
   context 'when data is supplied in the message body' do
     let(:oracle_adapter) do
-      OracleAdapterFake.new [
-        {
-          ASSESSOR_KEY: 89_030_012,
-          "ACTIVE_IND": 'N',
-          "ASSESSOR_ID": 'TEST000000',
-          "BASE_POSTCODE_LOCATION": 'S1 1DA',
-          "ADDRESS1": 'Green House',
-          "ADDRESS2": '89 Street',
-          "ADDRESS3": nil,
-          "POST_TOWN": 'Sheffield',
-          "POSTCODE": 'S1 2HE',
-          "COMPANY_NAME": 'Test Ltd',
-          "EMAIL": 'test@example.com',
-          "COMPANY_FAX": nil,
-          "TELEPHONE": '07777777777',
-          "COMPANY_WEBSITE": 'www.example.com',
-          "DATE_OF_BIRTH": '1980-11-01 00:00:00.000000',
-          "FIRST_NAME": 'Joe',
-          "NAME_PREFIX": 'Mr',
-          "NAME_SUFFIX": nil,
-          "SURNAME": 'Testerton',
-          "DELETED_IN_UPLOAD": 123_456,
-          "ORGANISATION_KEY": 144,
-          "FIRST_NAME_PHONIC": 'JM',
-          "SURNAME_PHONIC": 'TN',
-          "ASSESSOR_LOCATION": [
-            1234,
-            1234,
-            [
-              746_745,
-              523_646,
-              nil
-            ],
-            nil,
-            nil
-          ],
-          "MIDDLE_NAMES": 'Not Applicable',
-          "DISPLAY_NAME": nil,
-          "COMPANY_REG_NO": nil,
-          "COMPANY_ADDR1": 'Example address',
-          "COMPANY_ADDR2": 'Long Street',
-          "COMPANY_ADDR3": nil,
-          "COMPANY_TOWN": 'Sheffield',
-          "COMPANY_POSTCODE": 'S1 1DA',
-          "COMPANY_TELEPHONE": '07000 678901',
-          "COMPANY_EMAIL": 'test@example.com',
-          "UPI": nil
-        }
-      ]
+      OracleAdapterFake.new OracleAdapterStub.data
     end
 
     it 'extracts the data' do
