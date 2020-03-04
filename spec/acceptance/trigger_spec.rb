fcd25491a9d2d6a5bec4e0ee12f8e322fd94a771
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2020-03-04 11:13:58 +0000

    Raise a RequestWithoutBody error for an empty trigger message
    Co-authored-by: Yusuf <yusuf@madetech.com>

--- spec/acceptance/trigger_spec.rb
+++ spec/acceptance/trigger_spec.rb
@@ -1,6 +1,19 @@
 require 'rspec'
 
 describe 'Acceptance::Trigger' do
+  context 'when an empty trigger notification is received' do
+    it 'raises an error' do
+      event = JSON.parse File.open('spec/event/sns-empty-trigger-input.json').read
+
+      ENV['ETL_STAGE'] = 'trigger'
+
+      expect do
+        handler = Handler.new Container.new false
+        handler.process event: event
+      end.to raise_error instance_of Errors::RequestWithoutBody
+    end
+  end
+
   context 'when a trigger notification is received' do
     it 'handles the event without raising an error' do
       event = JSON.parse File.open('spec/event/sns-trigger-input.json').read
