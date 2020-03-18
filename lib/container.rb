ab7c8037205153443e95868142009a46d2209996
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2020-03-18 13:43:24 +0000

    Add missing log gateway and adapter to container bootstrap

--- lib/container.rb
+++ lib/container.rb
@@ -11,12 +11,15 @@ class Container
 
       sqs_adapter = Adapter::SqsAdapter.new sqs_client
       oracle_adapter = Adapter::OracleAdapter.new
+      logit_adapter = Adapter::LogitAdapter.new
 
       message_gateway = Gateway::MessageGateway.new(sqs_adapter)
       database_gateway = Gateway::DatabaseGateway.new(oracle_adapter)
+      log_gateway = Gateway::LogGateway.new(logit_adapter)
 
       @objects[:message_gateway] = message_gateway
       @objects[:database_gateway] = database_gateway
+      @objects[:log_gateway] = log_gateway
     end
   end
 
